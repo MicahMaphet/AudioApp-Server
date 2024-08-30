03c33904aba96426a006a20a9b0b0ca4d5921b12
MicahMaphet <micahmaphet@gmail.com>
2024-08-30 14:26:22 -0400

    added cors so react-native accepts requests

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors'; 
 import storage from './storage'
 
 const port = 3000;
 const app = express();
+
+app.use(cors());
 
 app.get('/tails', async (req, res) => {
     res.send(await storage.tails.get());
