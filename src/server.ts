90805aef9eb754745a0c88b0ccfefe8663219014
MicahMaphet <micahmaphet@gmail.com>
2024-08-30 20:20:20 -0400

    added public images directory for serving images to client

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import cors from 'cors'; 
 import storage from './storage'
+import path from 'path';
 
 const port = 3000;
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/tails', async (req, res) => {
     res.send(await storage.tails.get());
-})
+});
 
 console.log(`Listening on port ${port}`);
 app.listen(port);
