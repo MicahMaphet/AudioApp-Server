c4057f6bbaae9ccd1ff351c0572d413b8215ba65
Micah Maphet <micahmaphet@gmail.com>
2024-11-22 15:21:38 +0900

    created ModelService and instantiated it into server with demo run

--- src/models.ts
+++ src/models.ts
@@ -2,25 +2,23 @@ import { AutoTokenizer, AutoModelForCausalLM, PreTrainedModel, PreTrainedTokeniz
 import { exec } from 'child_process';
 import path from 'path';
 
-function generate(prompt: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        exec('python ' + path.join(__dirname, '../LLMs/model.py') + ` '${prompt}'`, (error: Error | null, stdout: string,
-            stderr: string) => {
-                if (error) {
-                    console.error(`Error: ${error.message}`);
-                    return;
+class ModelService {
+    generate(prompt: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            exec('python ' + path.join(__dirname, '../LLMs/model.py') + ` '${prompt}'`, (error: Error | null, stdout: string,
+                stderr: string) => {
+                    if (error) {
+                        console.error(`Error: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        // console.warn(`stderr: ${stderr}`);
+                    }
+                    resolve(stdout.trim());
                 }
-                if (stderr) {
-                    // console.warn(`stderr: ${stderr}`);
-                }
-                resolve(stdout.trim());
-            }
-        )
-    });
+            )
+        });
+    }
 }
 
-async function main() {
-    console.log(await generate('Hello there, '))
-}
-
-main();
+export default new ModelService();
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,15 @@ import express from 'express';
 import cors from 'cors'; 
 import storage from './storage'
 import path from 'path';
+import models from './models'
 
 const port = 3000;
 const app = express();
+
+// This takes a while and will only finish after everything else
+(async () => {
+    console.log(await models.generate('This is a story written by TinTails-1M:'));
+})();
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
@@ -13,5 +19,5 @@ app.get('/tails', async (req, res) => {
     res.send(await storage.tails.get());
 });
 
-console.log(`Listening on port ${port}`);
+console.log(`Listening on port ${port}\n`);
 app.listen(port);
